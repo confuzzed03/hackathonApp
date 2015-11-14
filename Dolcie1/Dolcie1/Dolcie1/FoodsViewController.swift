b3eac1dacc1720f75d3189c6524a6e87a847a50e
zhangm94 <zhangm94@gmail.com>
2015-11-14 16:53:01 -0500

    maps and pulling foods
    food

--- Dolcie1/Dolcie1/Dolcie1/FoodsViewController.swift
+++ Dolcie1/Dolcie1/Dolcie1/FoodsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftHTTP
 
 class FoodsViewController: UIViewController {
 
@@ -19,6 +20,28 @@ class FoodsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    var counter=0
+    
+    @IBAction func incrementCounter(sender: UIButton) {
+        counter++
+        
+        sender.setTitle(String(counter), forState: UIControlState.Normal)
+        
+        do {
+            let opt = try HTTP.GET("https://api.nutritionix.com/v1_1/search/donatos?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=0957869a&appKey=5162308af2fc1b259e1f36ed1894eb4c")
+            opt.start { response in
+                if let err = response.error {
+                    print("error: \(err.localizedDescription)")
+                    return //also notify app of failure as needed
+                }
+                print("opt finished: \(response.description[0].brand_id)")
+                //print("data is: \(response.data)") access the response of the data with response.data
+            }
+        } catch let error {
+            print("got an error creating the request: \(error)")
+        }
     }
     
 
